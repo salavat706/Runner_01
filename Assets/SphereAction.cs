a01e5afa9431cb8fbc90a8898033c5d6ffd5ffe5
salavat706 <38453102+salavat706@users.noreply.github.com>
2018-07-02 20:24:39 +0500

    1st commit
    Its so easy

---
+++ Assets/SphereAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereAction : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(Vector3.forward * Time.deltaTime * 5);
+        //transform.Translate(Vector3.forward * Time.deltaTime * 5);
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cube")
+        {
+            transform.position = new Vector3( 0, transform.position.y, 0);
+        }
+    }
+}
